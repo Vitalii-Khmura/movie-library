2a78fbdab87898debc7730be3b1d77648a769058
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-07-23 16:44:18 +0300

    Register models in movie/admin.py

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from movie.models import (
+    Movie,
+    Reviews,
+    Actor,
+    Genre
+)
+
+
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Reviews)
+admin.site.register(Actor)
+admin.site.register(Genre)
