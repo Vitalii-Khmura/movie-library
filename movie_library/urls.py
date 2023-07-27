b1385a583cef230235b4510920a5254544208c80
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-07-27 12:43:43 +0300

    Register static and media files

--- movie_library/urls.py
+++ movie_library/urls.py
@@ -1,8 +1,16 @@
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from movie_library import settings
+
 urlpatterns = [
-    path("admin", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("movie.urls", namespace="movie"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
