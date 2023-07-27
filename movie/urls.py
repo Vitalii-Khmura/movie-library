ced07d87697141e839d6158d5441e77391cb9650
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-07-27 12:59:19 +0300

    Created view and url for main page

--- movie/urls.py
+++ movie/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from movie.views import MovieListView
 
+urlpatterns = [
+    path("", MovieListView.as_view(), name="index"),
 ]
 
 app_name = "movie"
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View, generic
+
+from movie.models import Movie, Actor, Genre
+
 
 # Create your views here.
+class MovieListView(generic.ListView):
+    model = Movie
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context["actors"] = Actor.objects.all()
+        context["genres"] = Genre.objects.all()
+
+        return context
+
